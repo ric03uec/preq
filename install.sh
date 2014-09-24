1fb76c7af76eda4f6b88911e89a9365d6d051a0a
devashish <devashish.86@gmail.com>
2014-09-24 21:32:09 +0530

    updating instaler and binary

--- install.sh
+++ install.sh
@@ -1,13 +1,13 @@
 #!/bin/bash -e
 
+BINARY_PATH="https://raw.githubusercontent.com/ric03uec/tpr/v0.1.0/tpr"
+
 sys_has() {
     type "$1" > /dev/null 2>&1
     return $?
 }
 go_download() {
-  if sys_has "curl"; then
-    curl $*
-  elif sys_has "wget"; then
+  if sys_has "wget"; then
     ARGS=$(echo "$*" | sed -e 's/--progress-bar /--progress=bar /' \
                            -e 's/-L //' \
                            -e 's/-I /--server-response /' \
@@ -15,11 +15,13 @@ go_download() {
                            -e 's/-o /-O /' \
                            -e 's/-C - /-c /')
     wget $ARGS
+  else
+    echo "No wget found, please install wget or download the binary directly from $BINARY_PATH";
   fi
 }
 
 mkdir -p $HOME/.bin
-go_download
+go_download "$BINARY_PATH"
 mv tpr $HOME/.bin
 echo PATH=$PATH:$HOME/.bin >> $HOME/.bashrc
 . $HOME/.bashrc 2>&1 > /dev/null
