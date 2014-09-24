f20f61fda3e14f4b94b300951d57152de8a54573
devashish <devashish.86@gmail.com>
2014-09-24 21:06:01 +0530

    adding binary and initial install script

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -e
+
+sys_has() {
+    type "$1" > /dev/null 2>&1
+    return $?
+}
+go_download() {
+  if sys_has "curl"; then
+    curl $*
+  elif sys_has "wget"; then
+    ARGS=$(echo "$*" | sed -e 's/--progress-bar /--progress=bar /' \
+                           -e 's/-L //' \
+                           -e 's/-I /--server-response /' \
+                           -e 's/-s /-q /' \
+                           -e 's/-o /-O /' \
+                           -e 's/-C - /-c /')
+    wget $ARGS
+  fi
+}
